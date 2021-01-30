4ef4a91daf9f35ffdd7db39e4a5672432017bfa8
Igor <prezime@gmail.com>
2021-01-30 16:42:54 +0100

    static files update

--- src/settings.py
+++ src/settings.py
@@ -135,7 +135,7 @@ STATICFILES_DIRS = [
 STATICFILES_STOREGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'static/media/')
 X_FRAME_OPTIONS = 'SAMEORIGIN'
 
 SUMMERNOTE_CONFIG = {
