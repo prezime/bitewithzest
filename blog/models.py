f6083853bb32322a6ca22ad7ed64b7a84ae2e86c
Igor <prezime@gmail.com>
2021-01-31 19:34:53 +0100

    major admin changes

--- blog/models.py
+++ blog/models.py
@@ -78,8 +78,8 @@ for item in cats:
   category_list.append(item) 
 
 class Post(models.Model):
-    title = models.CharField(max_length=200, unique=True)
     titleplus = models.CharField(max_length=200, default='')
+    
     slug = models.SlugField(max_length=200, unique=True)
     author = models.ForeignKey(User, on_delete= models.CASCADE,related_name='blog_posts')
     updated_on = models.DateTimeField(auto_now= True)
@@ -88,20 +88,23 @@ class Post(models.Model):
     status = models.IntegerField(choices=STATUS, default=0)
     cardtype = models.IntegerField(choices=CARDTYPE, default=0)
 
-
-    intro = models.TextField()
-    intro1 = models.TextField(default='',blank=True)
-    maintext = models.TextField(default='')
-
-    preparationtext = models.TextField(default='',blank=True)
-    ingredients = models.TextField(default='',blank=True)
-
-    tip = models.TextField(default='',blank=True)
-    
     thumbnail = models.FileField(upload_to=path_and_rename,blank=True)
     intro_pic= models.FileField(upload_to=path_and_rename_intro,blank=True)
-    main_pic = models.FileField(upload_to=path_and_rename_main,blank=True)
+    intro = models.TextField()
+    title = models.CharField(max_length=200, unique=True)
+    maintext = models.TextField(default='')
+    intro1 = models.TextField(default='',blank=True)
+    legend = models.TextField(default='',blank=True)
     add_pic = models.FileField(upload_to=path_and_rename_add,blank=True)
+    ingredients = models.TextField(default='',blank=True)
+    preparationtext = models.TextField(default='',blank=True)
+    main_pic = models.FileField(upload_to=path_and_rename_main,blank=True)
+    tip = models.TextField(default='',blank=True)
+    
+    
+    
+    
+    
     
     class Meta:
         ordering = ['-created_on']
