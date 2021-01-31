fbe455c0c4ad794453e40f884b4303eaba44e26c
Igor <prezime@gmail.com>
2021-01-31 10:13:33 +0100

    reset last changes

--- blog/admin.py
+++ blog/admin.py
@@ -6,7 +6,7 @@ class PostAdmin(admin.ModelAdmin):
 #class PostAdmin(SummernoteModelAdmin):       
     list_display = ('title', 'slug', 'status','created_on')
     list_filter = ("status",'cardtype')
-    list_category = ('category')
+
     search_fields = ['title', 'intro', 'maintext']
     prepopulated_fields = {'slug': ('title',)}
     summernote_fields = ('intro','maintext','preparationtext','ingredients')
