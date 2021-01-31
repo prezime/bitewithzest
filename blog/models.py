ac867660fd79a09f8cc0830f503839b414da35cd
Igor <prezime@gmail.com>
2021-01-31 11:20:34 +0100

    categories tweaked1

--- blog/models.py
+++ blog/models.py
@@ -84,7 +84,7 @@ class Post(models.Model):
     author = models.ForeignKey(User, on_delete= models.CASCADE,related_name='blog_posts')
     updated_on = models.DateTimeField(auto_now= True)
     created_on = models.DateTimeField(auto_now_add=True)
-    category = models.CharField(max_length=200, default='')
+    category = models.CharField(max_length=200, choices=category_list, default='')
     status = models.IntegerField(choices=STATUS, default=0)
     cardtype = models.IntegerField(choices=CARDTYPE, default=0)
 
