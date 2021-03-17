678cc3a4af63023efc679a876e8abe252f56e393
Igor Sarkanovic <igorsarkanovic@Igors-MacBook-Pro.local>
2021-03-17 17:50:51 +0100

    STATIFILES_STORAGE corr.

--- src/settings.py
+++ src/settings.py
@@ -132,7 +132,7 @@ STATIC_ROOT  = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
-STATICFILES_STOREGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'static/media/')
